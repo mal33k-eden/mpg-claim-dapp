40c512f70b720d6a5f9236682908a615c27310cf
Emm Kwami <edenated@gmail.com>
2022-07-26 07:23:26 -0700

    rpc changed

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import { MoralisProvider } from 'react-moralis';
 import('./polyfill');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const appId = "IA1fZV3RqNGrDbAWRW82zaA2txBeGQK9EBs5QhKi"
-const serverUrl="https://xfqoi5qkxu9m.usemoralis.com:2053/server"
+const appId = "bpvMvN0ghqMNTjuhVeiw1rVKVUh7If5VNd0kxwUQ"
+const serverUrl="https://oazapcnygpfk.usemoralis.com:2053/server"
 
 
 root.render(
